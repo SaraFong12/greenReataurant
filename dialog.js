6b06de97c5b0157372dd910ad827d4278bb2ef7e
Sara <st51022sara@gmail.com>
2023-06-08 11:46:22 +0800

    新增網頁環境

---
+++ dialog.js
@@ -0,0 +1,176 @@
+$("#dl1")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl2")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl3")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl4")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl5")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl6")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl7")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl9")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl11")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl13")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl14")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl15")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl16")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl17")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl18")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
+    $("#dl19")
+    .dialog({
+        width:"700px",
+        autoOpen: false,
+        modal: true,
+        buttons:{
+            Ok: function(){
+                $(this).dialog("close")
+            }
+        }
+    })
